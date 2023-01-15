41878435cab9736d6b847c850f1980270eafea79
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2023-01-15 11:18:14 +0300

    Add navbar and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AuthGuardService } from './auth-guard.service';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { TransferComponent } from './transfer/transfer.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [{ path: 'transfer', component: TransferComponent }],
+    canActivate: [AuthGuardService],
+  },
+  { path: 'login', component: LoginComponent },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { AuthGuardService } from './auth-guard.service';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    TransferComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  providers: [AuthGuardService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
