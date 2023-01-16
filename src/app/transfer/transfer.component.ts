7aedea737e4d8123cc50206e429dfb3f730f1dfd
Ahmad AlAmiri <a.alamiri.96@gmail.com>
2023-01-16 09:53:34 +0300

    Add transfer page

--- src/app/transfer/transfer.component.ts
+++ src/app/transfer/transfer.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
-  styleUrls: ['./transfer.component.css']
+  styleUrls: ['./transfer.component.css'],
 })
-export class TransferComponent {
-
-}
+export class TransferComponent {}
